85de56fee2ff789c408be29438b942259202b33c
Adam <18970079817@189.cn>
2019-01-28 17:26:51 +0800

    Move files to the parent floder.

--- test_7_movefile.py
+++ test_7_movefile.py
@@ -1,6 +1,6 @@
 import shutil as s
 import os
-
+#move files to the parent folder
 pwd = os.getcwd()
 floder = os.listdir(pwd)
 floder.remove('test_7_movefile.py')
@@ -8,5 +8,8 @@ floder.remove('test_7_movefile.py')
 for i in floder:
     os.chdir(i)
     for f in os.listdir('.'):
-        s.move('.', pwd)
+        oldpath = os.path.abspath(f)
+        newpath = os.path.abspath(os.path.join(os.getcwd(), '..'))
+        newpath = os.path.join(newpath, f)
+        s.move(oldpath, newpath)
     os.chdir(pwd)
